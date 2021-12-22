4bcf1d7a1fe6f8b6ec6f1fa7b8e4d08cef3c7ac7
LatvianModder <latvianmodder@gmail.com>
2021-12-22 20:43:19 +0200

    Added heat exchanger info

--- src/main/java/dev/ftb/mods/ftbic/item/reactor/HeatExchangerItem.java
+++ src/main/java/dev/ftb/mods/ftbic/item/reactor/HeatExchangerItem.java
@@ -1,12 +1,35 @@
 package dev.ftb.mods.ftbic.item.reactor;
 
+import dev.ftb.mods.ftbic.util.FTBICUtils;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
 public class HeatExchangerItem extends BaseReactorItem {
-	public final int heatTransferToAdjecent;
+	public final int heatTransferToAdjacent;
 	public final int heatTransferToCore;
 
 	public HeatExchangerItem(int durability, int a, int c) {
 		super(durability);
-		heatTransferToAdjecent = a;
+		heatTransferToAdjacent = a;
 		heatTransferToCore = c;
+	}
+
+	@Override
+	public void reactorInfo(ItemStack stack, List<Component> list, boolean shift, boolean advanced, @Nullable NuclearReactor reactor, int x, int y) {
+		if (stack.getMaxDamage() > 0) {
+			list.add(new TextComponent("Coolant: ").append(FTBICUtils.formatHeat(stack.getMaxDamage() - stack.getDamageValue())).withStyle(ChatFormatting.GRAY));
+		}
+
+		list.add(new TextComponent("Heat Transfer to Adjacent: ").append(FTBICUtils.formatHeat(heatTransferToAdjacent)).append("/s").withStyle(ChatFormatting.GRAY));
+		list.add(new TextComponent("Heat Transfer to Core: ").append(FTBICUtils.formatHeat(heatTransferToCore)).append("/s").withStyle(ChatFormatting.GRAY));
+	}
+
+	@Override
+	public void reactorTickPre(NuclearReactor reactor, ItemStack stack, int x, int y) {
 	}
 }
