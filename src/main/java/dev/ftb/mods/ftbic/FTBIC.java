1c59ba43bf58f2f50961ad011b911bd80c077b3c
Michael Hillcox <michaelhillcox@gmail.com>
2022-04-28 21:16:42 +0100

    feat: ore generation

--- src/main/java/dev/ftb/mods/ftbic/FTBIC.java
+++ src/main/java/dev/ftb/mods/ftbic/FTBIC.java
@@ -13,23 +13,28 @@ import dev.ftb.mods.ftbic.recipe.FTBICRecipes;
 import dev.ftb.mods.ftbic.screen.FTBICMenus;
 import dev.ftb.mods.ftbic.sound.FTBICSounds;
 import dev.ftb.mods.ftbic.util.FTBICUtils;
+import dev.ftb.mods.ftbic.world.OreGeneration;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.levelgen.GenerationStep;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
+import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import org.apache.logging.log4j.LogManager;
@@ -73,6 +78,8 @@ public class FTBIC {
 		ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, FTBICConfig.COMMON_CONFIG);
 		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+		modEventBus.addListener(this::setup);
+
 		// Register all the registries
 		REGISTERS.forEach(e -> e.register(modEventBus));
 
@@ -80,7 +87,22 @@ public class FTBIC {
 		FTBICUtils.init();
 		FTBICNet.init();
 		FTBICConfig.init();
+
 		PROXY.init();
+	}
+
+	private void setup(final FMLCommonSetupEvent event) {
+		event.enqueueWork(OreGeneration::setupConfiguredFeatures);
+	}
+
+	@SubscribeEvent
+	public static void biomeLoadEvent(BiomeLoadingEvent event) {
+		var biome = event.getCategory();
+		if (biome == Biome.BiomeCategory.THEEND || biome == Biome.BiomeCategory.NONE || biome == Biome.BiomeCategory.NETHER) {
+			return;
+		}
+
+		OreGeneration.PLACEMENT_FEATURES.forEach((k, v) -> v.forEach(e -> event.getGeneration().addFeature(GenerationStep.Decoration.UNDERGROUND_ORES, e)));
 	}
 
 	private static boolean isDummyArmor(LivingDamageEvent event, EquipmentSlot slot, ArmorMaterial material) {
