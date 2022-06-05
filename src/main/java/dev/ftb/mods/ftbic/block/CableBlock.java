32f64618f9381487f74753fbefc5d2a9476fc09b
Max <maxh2709@gmail.com>
2022-06-05 18:46:40 +0200

    Stop FTB:IC Cables from connecting to FE machines that don't allow input

--- src/main/java/dev/ftb/mods/ftbic/block/CableBlock.java
+++ src/main/java/dev/ftb/mods/ftbic/block/CableBlock.java
@@ -24,6 +24,7 @@ import net.minecraft.world.level.material.Fluids;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.energy.CapabilityEnergy;
+import net.minecraftforge.energy.IEnergyStorage;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
@@ -62,8 +63,10 @@ public class CableBlock extends BaseCableBlock {
 		} else if (state.getBlock() instanceof ElectricBlock) {
 			return true;
 		} else if (!state.isAir()) {
-			BlockEntity t = world.getBlockEntity(pos);
-			return t instanceof EnergyHandler || FTBICConfig.ENERGY.ZAP_TO_FE_CONVERSION_RATE.get() > 0D && t != null && t.getCapability(CapabilityEnergy.ENERGY, face).isPresent();
+			BlockEntity be = world.getBlockEntity(pos);
+			if (FTBICConfig.ENERGY.ZAP_TO_FE_CONVERSION_RATE.get() > 0D && be instanceof EnergyHandler) {
+				return be.getCapability(CapabilityEnergy.ENERGY, face).map(IEnergyStorage::canReceive).isPresent();
+			}
 		}
 
 		return false;
