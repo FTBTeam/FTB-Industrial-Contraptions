6223ce468b2e3ec8a0ef155aabcae72ec55e3f5f
Michael Hillcox <michaelhillcox@gmail.com>
2022-06-09 20:11:50 +0100

    fix: added back the rubber mod and fixed a couple of issues

--- src/main/java/dev/ftb/mods/ftbic/datagen/FTBICRecipesGen.java
+++ src/main/java/dev/ftb/mods/ftbic/datagen/FTBICRecipesGen.java
@@ -6,6 +6,7 @@ import dev.ftb.mods.ftbic.item.FTBICItems;
 import dev.ftb.mods.ftbic.item.FluidCellItem;
 import dev.ftb.mods.ftbic.world.ResourceElements;
 import dev.ftb.mods.ftbic.world.ResourceType;
+import io.alwa.mods.myrtrees.common.item.MyrtreesItems;
 import net.minecraft.core.Registry;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.recipes.FinishedRecipe;
@@ -125,11 +126,11 @@ public abstract class FTBICRecipesGen extends RecipeProvider implements IConditi
 	public static final TagKey<Item> BLAZE_ROD = Tags.Items.RODS_BLAZE;
 	public static final TagKey<Item> LAPIS = Tags.Items.GEMS_LAPIS;
 	public static final Item PISTON = Items.PISTON;
-	public static final Item LATEX = Items.PAPER;//MyrtreesItems.LATEX.get(); // FIXME: add back (1.19 mod needed from jake)
-	public static final Item TREE_TAP = Items.PAPER;//MyrtreesItems.TREE_TAP.get(); // FIXME: add back (1.19 mod needed from jake)
-	public static final Item RUBBERWOOD_LOG = Items.PAPER;//MyrtreesItems.RUBBERWOOD_LOG.get(); // FIXME: add back (1.19 mod needed from jake)
-	public static final Item RUBBERWOOD_SAPLING = Items.PAPER;//MyrtreesItems.RUBBERWOOD_SAPLING.get(); // FIXME: add back (1.19 mod needed from jake)
-	public static final Item RUBBERWOOD_LEAVES = Items.PAPER;//MyrtreesItems.RUBBERWOOD_LEAVES.get(); // FIXME: add back (1.19 mod needed from jake)
+	public static final Item LATEX = MyrtreesItems.LATEX.get();
+	public static final Item TREE_TAP = MyrtreesItems.TREE_TAP.get();
+	public static final Item RUBBERWOOD_LOG = MyrtreesItems.RUBBERWOOD_LOG.get();
+	public static final Item RUBBERWOOD_SAPLING = MyrtreesItems.RUBBERWOOD_SAPLING.get();
+	public static final Item RUBBERWOOD_LEAVES = MyrtreesItems.RUBBERWOOD_LEAVES.get();
 	public static final TagKey<Item> IRON_ROD = TagKey.create(Registry.ITEM_REGISTRY, new ResourceLocation("forge", "rods/iron"));
 	public static final TagKey<Item> IRON_DUST = TagKey.create(Registry.ITEM_REGISTRY, new ResourceLocation("forge", "dusts/iron"));
 	public static final TagKey<Item> LEAD_DUST = TagKey.create(Registry.ITEM_REGISTRY, new ResourceLocation("forge", "dusts/lead"));
