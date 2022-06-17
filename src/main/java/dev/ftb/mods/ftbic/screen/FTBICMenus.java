8cf2b81d2ed9f09af05fcb67d12b0f5dfc21feae
Max <maxh2709@gmail.com>
2022-06-17 12:26:15 +0200

    (backport) Update JEI plugin to support newer versions of the API

--- src/main/java/dev/ftb/mods/ftbic/screen/FTBICMenus.java
+++ src/main/java/dev/ftb/mods/ftbic/screen/FTBICMenus.java
@@ -6,24 +6,25 @@ import net.minecraft.world.inventory.MenuType;
 import net.minecraftforge.network.IContainerFactory;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 import java.util.function.Supplier;
 
 public interface FTBICMenus {
 	DeferredRegister<MenuType<?>> REGISTRY = DeferredRegister.create(ForgeRegistries.CONTAINERS, FTBIC.MOD_ID);
 
-	static <T extends AbstractContainerMenu> Supplier<MenuType<T>> register(String id, IContainerFactory<T> factory) {
+	static <T extends AbstractContainerMenu> RegistryObject<MenuType<T>> register(String id, IContainerFactory<T> factory) {
 		return REGISTRY.register(id, () -> new MenuType<>(factory));
 	}
 
-	Supplier<MenuType<MachineMenu>> MACHINE = register("machine", MachineMenu::new);
-	Supplier<MenuType<BasicGeneratorMenu>> BASIC_GENERATOR = register("basic_generator", BasicGeneratorMenu::new);
-	Supplier<MenuType<GeothermalGeneratorMenu>> GEOTHERMAL_GENERATOR = register("geothermal_generator", GeothermalGeneratorMenu::new);
-	Supplier<MenuType<SolarPanelMenu>> SOLAR_PANEL = register("solar_panel", SolarPanelMenu::new);
-	Supplier<MenuType<NuclearReactorMenu>> NUCLEAR_REACTOR = register("nuclear_reactor", NuclearReactorMenu::new);
-	Supplier<MenuType<BatteryBoxMenu>> BATTERY_BOX = register("battery_box", BatteryBoxMenu::new);
-	Supplier<MenuType<AntimatterConstructorMenu>> ANTIMATTER_CONSTRUCTOR = register("antimatter_constructor", AntimatterConstructorMenu::new);
-	Supplier<MenuType<PoweredCraftingTableMenu>> POWERED_CRAFTING_TABLE = register("powered_crafting_table", PoweredCraftingTableMenu::new);
-	Supplier<MenuType<QuarryMenu>> QUARRY = register("quarry", QuarryMenu::new);
-	Supplier<MenuType<PumpMenu>> PUMP = register("pump", PumpMenu::new);
+	RegistryObject<MenuType<MachineMenu>> MACHINE = register("machine", MachineMenu::new);
+	RegistryObject<MenuType<BasicGeneratorMenu>> BASIC_GENERATOR = register("basic_generator", BasicGeneratorMenu::new);
+	RegistryObject<MenuType<GeothermalGeneratorMenu>> GEOTHERMAL_GENERATOR = register("geothermal_generator", GeothermalGeneratorMenu::new);
+	RegistryObject<MenuType<SolarPanelMenu>> SOLAR_PANEL = register("solar_panel", SolarPanelMenu::new);
+	RegistryObject<MenuType<NuclearReactorMenu>> NUCLEAR_REACTOR = register("nuclear_reactor", NuclearReactorMenu::new);
+	RegistryObject<MenuType<BatteryBoxMenu>> BATTERY_BOX = register("battery_box", BatteryBoxMenu::new);
+	RegistryObject<MenuType<AntimatterConstructorMenu>> ANTIMATTER_CONSTRUCTOR = register("antimatter_constructor", AntimatterConstructorMenu::new);
+	RegistryObject<MenuType<PoweredCraftingTableMenu>> POWERED_CRAFTING_TABLE = register("powered_crafting_table", PoweredCraftingTableMenu::new);
+	RegistryObject<MenuType<QuarryMenu>> QUARRY = register("quarry", QuarryMenu::new);
+	RegistryObject<MenuType<PumpMenu>> PUMP = register("pump", PumpMenu::new);
 }
