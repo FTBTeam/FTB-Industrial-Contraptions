28d818243bf7b47f5f7296f90f8b66ddb066b1d1
Saereth <paulanthonydevs@gmail.com>
2026-04-13 23:20:02 -0700

    use ItemStackTemplate for StackWithChance and simplify coolant cell recipe

--- src/main/java/dev/ftb/mods/ftbic/util/StackWithChance.java
+++ src/main/java/dev/ftb/mods/ftbic/util/StackWithChance.java
@@ -6,17 +6,16 @@ import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ItemStackTemplate;
 
-/**
- * An output ItemStack paired with a drop chance [0.0, 1.0]. Chance of 1.0 is a guaranteed output.
- */
 public record StackWithChance(ItemStack stack, double chance) {
 	public static final StackWithChance EMPTY = new StackWithChance(ItemStack.EMPTY, 0D);
 
 	public static final Codec<StackWithChance> CODEC = RecordCodecBuilder.create(i -> i.group(
-			ItemStack.CODEC.fieldOf("item").forGetter(StackWithChance::stack),
+			ItemStackTemplate.CODEC.fieldOf("item").forGetter(swc ->
+					new ItemStackTemplate(swc.stack().typeHolder(), swc.stack().getCount(), swc.stack().getComponentsPatch())),
 			Codec.DOUBLE.optionalFieldOf("chance", 1D).forGetter(StackWithChance::chance)
-	).apply(i, StackWithChance::new));
+	).apply(i, (template, chance) -> new StackWithChance(template.create(), chance)));
 
 	public static final StreamCodec<RegistryFriendlyByteBuf, StackWithChance> STREAM_CODEC = StreamCodec.composite(
 			ItemStack.STREAM_CODEC, StackWithChance::stack,
